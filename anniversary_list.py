77bd90ab8cb417befaa3a2db7c653138f9c2bd19
Muppet Gate <muppet.gate@yahoo.com>
2014-07-01 20:09:58 +0100

    Fixes

--- anniversary_list.py
+++ anniversary_list.py
@@ -5,25 +5,42 @@ import sys
 
 from date_format_mappings import DATE_MAPPINGS, DEFAULT_WORKFLOW_SETTINGS
 from versioning import update_settings
-from workflow import Workflow
+from workflow import Workflow, MATCH_STARTSWITH
 from dateutil import parser
+
+
+def filter_names(name):
+    """
+    This is just the filter function
+    for the framework search
+    :param name:
+    :return:
+    """
+    return name.lower()
 
 
 def main(wf):
     # Get the date format from the configuration
 
     update_settings(wf)
+    args = wf.args
 
     key = wf.settings['date-format']
 
     date_mapping = DATE_MAPPINGS[key]
     anniversaries = wf.settings["anniversaries"]
 
-    for anniversary in sorted(anniversaries.keys()):
+    if args[0]:
+        filtered_keys = wf.filter(query=args[0], items=anniversaries.keys(),
+                                  key=filter_names, match_on=MATCH_STARTSWITH)
+    else:
+        filtered_keys = anniversaries.keys()
+
+    for anniversary in sorted(filtered_keys):
         anniversary_date_str = anniversaries[anniversary]
         date_object = parser.parse(anniversary_date_str)
         entry = "{anniversary} ➤ {date}".format(anniversary=anniversary,
-                                                 date=date_object.strftime(date_mapping['date-format']))
+                                                  date=date_object.strftime(date_mapping['date-format']))
 
         wf.add_item(title=entry, subtitle="Press alt to change, ctrl to delete", arg=anniversary, valid=True)
 
